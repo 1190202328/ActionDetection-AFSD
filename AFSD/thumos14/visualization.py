a8f83522f459387dceb3ab736dae0004a2b9b9e3
1190202328 <1259029328@qq.com>
2022-06-18 10:09:40 +0800

    v1.0 可视化成功

--- AFSD/thumos14/visualization.py
+++ AFSD/thumos14/visualization.py
@@ -15,7 +15,7 @@ from mpl_toolkits import axisartist
 to_visualize_name = {
     'video_test_0000006': [19, 18.8, 57.3, 'VolleyballSpiking', 564, 1719],  # 类别id，开始秒数，结束秒数，类别名称，开始帧，结束帧
     'video_test_0000011': [15, 0.4, 12.4, 'Shotput', 12, 372],
-    'video_test_0000058': [3, 1.9, 26.6, 'ThrowDiscus', 57, 798],
+    'video_test_0000058': [3, 1.9, 26.6, 'CleanAndJerk', 57, 798],
     'video_test_0000113': [9, 60.8, 79, 'GolfSwing', 1824, 2370],
 }
 
@@ -46,15 +46,14 @@ def get_data():
     os.makedirs(output_dir)
 
     results = load_results('../../output/thumos14_fusion.json')
-    label2id = load_label2id('../../thumos_annotations/Class Index_Detection.txt')
 
     for vid in to_visualize_name:
         draw(results[vid], to_visualize_name[vid], f'{video_dir}/{vid}.mp4',
-             f'{output_dir}/{vid}.jpg', label2id)
+             f'{output_dir}/{vid}.jpg')
         print(f'{vid}.jpg generated!')
 
 
-def draw(result, info, video_path, output_file, label2id):
+def draw(result, info, video_path, output_file):
     # 绘图
     total_num = 1000
     end = 100
@@ -63,30 +62,23 @@ def draw(result, info, video_path, output_file, label2id):
     factor = total_num / ((info[2] - info[1]) + 2 * bound)
     left = info[1] - bound
     right = info[2] + bound
-    label_id = info[0]
-    class_name = info[3]
+    label_name = info[3]
     #
     x = np.linspace(0, end, total_num)
     y_onset = np.zeros(shape=x.shape)
     y_offset = np.zeros(shape=x.shape)
     for i in range(len(result)):
-        onset, offset = result[i]['segment']
-        score = result[i]['score']
-        label = label2id[result[i]['label']]
-        if left <= onset <= right and label == label_id:
+        onset, offset = [float(i) for i in result[i]['segment']]
+        score = float(result[i]['score'])
+        label = result[i]['label']
+        if left <= onset <= right and label == label_name:
             y_onset[int((onset - left) * factor)] += score
-        if left <= offset <= right and label == label_id:
+        if left <= offset <= right and label == label_name:
             y_offset[int((offset - left) * factor)] += score
-    y_point = np.zeros(shape=x.shape)
-    for i in range(len(result['point'])):
-        point, label, score = result['point'][i]
-        if left <= point <= right and label == label_id:
-            y_point[int((point - left) * factor)] += score
-
     # 1.画最上面的图片
-    fig = plt.figure(figsize=(20, 12))
+    fig = plt.figure(figsize=(20, 8))
 
-    ax = axisartist.Subplot(fig, 311)
+    ax = axisartist.Subplot(fig, 211)
     fig.add_axes(ax)
     try:
         video = imageio.get_reader(video_path, 'ffmpeg')
@@ -116,32 +108,8 @@ def draw(result, info, video_path, output_file, label2id):
     except FileNotFoundError:
         pass
 
-    # 2.画中间的classification
-    ax = axisartist.Subplot(fig, 312)
-    # 将绘图区对象添加到画布中
-    fig.add_axes(ax)
-    x_set = MultipleLocator(end / total_num * 50)  # x轴的刻度
-    # 设置刻度间隔
-    ax.xaxis.set_major_locator(x_set)
-    gt_x_onset = np.ones(shape=(10,)) * (info[1] - left) / ((info[2] - info[1]) + 2 * bound) * end
-    gt_x_offset = np.ones(shape=(10,)) * (info[2] - left) / ((info[2] - info[1]) + 2 * bound) * end
-    max_y = max(y_point) * 1.1
-    gt_y = np.linspace(0, max_y, 10)
-    ax.plot(gt_x_onset, gt_y, c='black', linestyle='--')
-    ax.plot(gt_x_offset, gt_y, c='black', linestyle='--')
-    ax.bar(x, y_point, color='red', label=f'Classification Scores for "{class_name}"')
-    # # 设置图表参数
-    ax.axis["bottom"].set_axisline_style("->", size=1.5)
-    ax.axis["left"].set_axisline_style("->", size=1.5)
-    # 通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
-    ax.axis["top"].set_visible(False)
-    ax.axis["right"].set_visible(False)
-    plt.xticks([])
-    plt.yticks([])
-    plt.legend(loc='upper right')
-
-    # 3.画下面的regression
-    ax = axisartist.Subplot(fig, 313)
+    # 2.画下面的regression
+    ax = axisartist.Subplot(fig, 212)
     # 将绘图区对象添加到画布中
     fig.add_axes(ax)
     x_set = MultipleLocator(end / total_num * 50)  # x轴的刻度
